14422ccd748aa03f98b36a87224ac2dcf4da797d
Alexander Heffernan <heffernan.alexander@icloud.com>
2025-04-20 15:15:32 +1200

    Setting up frontend building.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/ServerWatch/', // Matches GitHub Pages URL
+  outputDir: path.resolve(__dirname, '../dist'), // Output to repo root
 })
